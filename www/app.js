8e1314317886995bc671e2178d9077e0a542477e
rajatinvoice@gmail.com <rajatinvoice@gmail.com>
2019-02-21 19:29:57 +0530

    Rajat

--- www/app.js
+++ www/app.js
@@ -1,9 +1,9 @@
-// var project_name = '/focusweb/webservices/Webapi';
-var project_name = '/Projects2018/pioneer/webservices/Webapi';
+var project_name = '/focus/webservices/Webapi';
+// var project_name = '/Projects2018/pioneer/webservices/Webapi';
 var country = 'en';
-// var base_url = 'http://192.168.240.1'
+var base_url = 'http://projects.tekshapers.in'
 // var base_url = 'http://192.168.1.43'
-var base_url = 'http://192.168.31.199'
+// var base_url = 'http://192.168.31.199'
 var WebUrl = base_url + project_name;
 var app_upload_url = base_url + project_name;
 var app_url = base_url + project_name;
